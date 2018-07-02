453694050dc3e161861db8712944539eccb2bc8d
Steve Smith <steve@kentsmiths.com>
2018-07-02 09:50:04 -0400

    Updating README and initial package.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ValidationRules
+
 A collection of validation rules for VS Web Tests
+
+Depends on Microsoft.VisualStudio.QualityTools.WebTestFramework.dll.
+
+## Usage
+
+After adding a reference to the package, you should see new validation rule(s) appear when you add a new Validation Rule to a web test in Visual Studio. The current list of validation rules is shown below.
+
+### JsonPropertyValidationRule
+
+The JSON Property validation rule lets you specify the path to a JSON property and its expected value. The rule passes if the response is a JSON response and it includes the specified property and it has the specified value.
---
+++ src/ValidationRules/JsonPropertyValidationRule.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.WebTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel;
+
+namespace Ardalis.ValidationRules
+{
+    /// <summary>
+    /// A JSON validation rule for Visual Studio webtest that validates a property of a JSON response.
+    /// Source: https://gist.github.com/LockTar/1a90d92335d524f26604
+    /// </summary>
+    [DisplayName("JSON Property Validation Rule")]
+    [Description("Validates the value from a JSON property in the response")]
+    public class JsonPropertyValidationRule : ValidationRule
+    {
+        /// <summary>
+        /// The name of the JSON property to validate in the JSON result.
+        /// </summary>
+        [DisplayName("Property name")]
+        [Description("The name of the JSON property to validate in the JSON result")]
+        public string JSonPropertyName { get; set; }
+
+        /// <summary>
+        /// The expected value of the property in the JSON result.
+        /// </summary>
+        [DisplayName("Expected value")]
+        [Description("The expected value of the property in the JSON result")]
+        public string ExpectedValue { get; set; }
+
+        /// <summary>
+        /// Validates if the property is available in the response and if it is the same as the expected value.
+        /// </summary>
+        public override void Validate(object sender, ValidationEventArgs e)
+        {
+            var jsonBody = JsonConvert.DeserializeObject(e.Response.BodyString);
+            var o = JObject.FromObject(jsonBody);
+            string value = (string)o.SelectToken(JSonPropertyName);
+
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                if (value == ExpectedValue)
+                {
+                    e.Message = $"Property '{JSonPropertyName}' has the value '{value}' and is the same as the expected value '{ExpectedValue}'";
+                    e.IsValid = true;
+                }
+                else
+                {
+                    e.Message = $"Property '{JSonPropertyName}' with value '{value}' is not the same as expected value '{ExpectedValue}'";
+                    e.IsValid = false;
+                }
+            }
+            else
+            {
+                e.Message = $"Property '{JSonPropertyName}' not found in response";
+                e.IsValid = false;
+            }
+        }
+    }
+}
